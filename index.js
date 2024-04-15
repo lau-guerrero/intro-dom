424fb935d1afc62553fb6e45f03bfd951231c8a3
lau-guerrero <laura.guerrero.gm@gmail.com>
2024-04-15 10:38:41 +0200

    correction

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let squaresContainer = document.querySelector(".squaresContainer");
+let squareHTML = "<div class='redSquare'></div>"
+
 function change() {
     // add red squares to the HTML when the button is clicked
+   
+    // squaresContainer.innerHTML += "<div class='redSquare'></div>"
+    squaresContainer.innerHTML += squareHTML
+
 }
